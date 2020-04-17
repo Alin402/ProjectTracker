91ce27a35c3e10686f86ac31262c27b017170366
Alin402 <balanescu.alin03@gmail.com>
2020-04-17 12:12:21 +0300

    Updates

---
+++ precache-manifest.b3866f0d9ee434aa26a29a9cf460d1ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1eacb7070dc751a90142378ee361b4a",
+    "url": "/ProjectTracker/index.html"
+  },
+  {
+    "revision": "422e3c51c513eccb1882",
+    "url": "/ProjectTracker/static/css/main.8d2427b3.chunk.css"
+  },
+  {
+    "revision": "c04989fd22ae33cb0f57",
+    "url": "/ProjectTracker/static/js/2.2e0ff332.chunk.js"
+  },
+  {
+    "revision": "e293c143e177a32373554d6ca4c08bba",
+    "url": "/ProjectTracker/static/js/2.2e0ff332.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "422e3c51c513eccb1882",
+    "url": "/ProjectTracker/static/js/main.869c6281.chunk.js"
+  },
+  {
+    "revision": "0e53c1ed76401812b16f",
+    "url": "/ProjectTracker/static/js/runtime-main.514ebac1.js"
+  }
+]);
